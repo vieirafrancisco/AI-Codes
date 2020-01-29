4cf2b79bfeef2cc62a3b26ee6ae682c7c2f31887
Francisco Neto <fvcneto.master98@gmail.com>
2020-01-29 11:43:19 -0300

    train method initial implemantation

--- NeuralNetwork/MLP/mlp.py
+++ NeuralNetwork/MLP/mlp.py
@@ -2,17 +2,31 @@ from resources.matrix import Matrix
 from resources.functions import sigmoid
 
 class MLP:
-    def __init__(self, layers, learning_rate=0.1):
+    def __init__(self, layers, learning_rate=0.1, epochs=100, verbose=False):
         self.layers = layers
         self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.verbose = verbose
         self.weights = [Matrix(y, x, True) for x, y in zip(layers[:-1], layers[1:])]
         self.bias = [Matrix(y, 1, True) for y in layers[1:]]
 
     def feedfoward(self, input_array):
-        input = Matrix.from_array(input_array)
+        data = Matrix.from_array(input_array)
         for w, b in zip(self.weights, self.bias):
-            input = Matrix.map(w * input + b, sigmoid)
-        return input
+            data = Matrix.map(w * data + b, sigmoid)
+        return data.to_array()
+
+    def train(self, x_train, y_train):
+        for epoch in range(self.epochs):
+            for x, y in zip(x_train, y_train):
+                output = self.feedfoward(x_train)
+
+                ## compare output with de answer
+
+                ## update weights and bias
+
+        if self.verbose:
+            pass
 
 
 if __name__ == '__main__':
